1ea196d5b602c50c976cea4f7b4c595b8cfe7124
ScaccoFranco <samuelefrancoo@gmail.com>
2025-09-23 14:57:53 +0200

    pulizia automatica

--- user_system/apps/authentication/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/', views.LoginView.as_view(), name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('2fa/setup/', views.setup_two_factor, name='setup_2fa'),
-    path('2fa/verify/', views.verify_two_factor, name='verify_2fa'),
-]
